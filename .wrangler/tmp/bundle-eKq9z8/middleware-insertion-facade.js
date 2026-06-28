453d05be94d896548f601432c1372c497b48540a
Tran Quoc Hoai <bsquochoainew@gmail.com>
2026-06-28 16:36:48 +0700

    sad

---
+++ .wrangler/tmp/bundle-eKq9z8/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "D:\\_VSCODE_Git\\cloudflare-workers-D1-astro-decapcms-wordpress-replacement\\src\\workers\\index.js";
+				import * as __MIDDLEWARE_0__ from "D:\\_VSCODE_Git\\cloudflare-workers-D1-astro-decapcms-wordpress-replacement\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\_VSCODE_Git\\cloudflare-workers-D1-astro-decapcms-wordpress-replacement\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "D:\\_VSCODE_Git\\cloudflare-workers-D1-astro-decapcms-wordpress-replacement\\src\\workers\\index.js";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://',
+  site: 'https://my-astro-site.vnvdacloudlear.workers.dev/',
   vite: {
     plugins: [tailwindcss()]
   },
